80bf38df366dbb7f89819e44e6e4ed1a9f816760
Lide Cui <bizarrejazzydede@gmail.com>
2024-11-07 05:56:11 +0800

    test amplify

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Amplify } from 'aws-amplify'
+import amplifyconfig from './amplifyconfiguration.json'
+
+Amplify.configure(amplifyconfig)
 
 const app = createApp(App)
 
